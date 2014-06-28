ddbd0c08dbab6e532f2ae3bfe6363855dd21123c
gchas <gchas@soft-project.pl>
2014-06-28 13:22:33 +0200

    Zmiana klasy

--- src/pl/altkom/phonebook/Start.java
+++ src/pl/altkom/phonebook/Start.java
@@ -3,7 +3,7 @@ package pl.altkom.phonebook;
 public class Start {
 
     public static void main(String[] args) {
-	// TODO Auto-generated method stub
+	// TODO Auto-generated method stub//
 
     }
 
